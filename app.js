62aca2d75474f6d4bc1705f8f605a1e5effb9b84
Abdullahi Elmi <abdullahielmi147@gmail.com>
2022-09-21 17:16:12 -0700

    part C search finished

--- app.js
+++ app.js
@@ -37,8 +37,8 @@ window.addEventListener('load', async () => {
 
 async function findCountries(name, continent) {
     // > Part A: Call the service function that gets the countries
-    const response = await getCountries(name, continent);
     // > Part C: Add the name and continent arguments to getCountries
+    const response = await getCountries(name, continent);
 
     // > Part A: Assign to state the :
     //      - error,
@@ -58,6 +58,7 @@ searchForm.addEventListener('submit', (e) => {
     e.preventDefault();
     const formData = new FormData(searchForm);
     // > Part C: Call findCountries with name and continent from formData
+    findCountries(formData.get('name'), formData.get('continent'));
 });
 
 /* Display Functions */
